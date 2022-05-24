fdf2326315034aae62c1f340ccc629bcdccfd5c9
Simon Adorf <carl.simon.adorf@gmail.com>
2022-05-24 13:56:06 +0200

    Add pre-commit hook to sync openapi.json.

--- marketplace_standard_app_api/__main__.py
+++ marketplace_standard_app_api/__main__.py
@@ -14,9 +14,27 @@ def cli():
 
 
 @cli.command()
-def show():
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(dir_okay=False, writable=True, allow_dash=True),
+    default="-",
+    help="Output file.",
+)
+def show(output):
     """Print the API as OpenAPI JSON document to stdout."""
-    click.echo(json.dumps(api.openapi(), indent=2))
+    if output == "-":
+        click.echo(json.dumps(api.openapi(), indent=2))
+    else:
+        # We only write the file if it either does not exist or in case it
+        # actually differs to avoid modifying the file metadata on every commit.
+        try:
+            current_api = click.open_file(output, "r").read()
+        except FileNotFoundError:
+            current_api = None
+        new_api = json.dumps(api.openapi(), indent=2) + "\n"
+        if current_api != new_api:
+            click.open_file(output, "w").write(new_api)
 
 
 if __name__ == "__main__":
