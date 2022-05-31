2d4eb1c9f5bb167d21fd90c06d6f1d85cf666ff8
nash <62147380+yoavnash@users.noreply.github.com>
2022-05-31 16:44:46 +0200

    Add message broker models (#5)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ marketplace_standard_app_api/models/message_broker.py
@@ -0,0 +1,16 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, Field
+
+
+class MessageBrokerRequestModel(BaseModel):
+    endpoint: str = Field(None, description="API endpoint of the application")
+    query_params: Optional[Dict[str, str]] = Field(None, description="Query parameters")
+    headers: Optional[Dict[str, str]] = Field(None, description="Request headers")
+    body: Optional[str] = Field(None, description="The request message body")
+
+
+class MessageBrokerResponseModel(BaseModel):
+    status_code: int = Field(200, description="The HTTP response code")
+    headers: Optional[Dict[str, str]] = Field(None, description="Response headers")
+    body: Optional[str] = Field(None, description="The response message body")
