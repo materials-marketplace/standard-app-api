d72025d9abc138f17dac7d77e089b373ff5e2347
nash <62147380+yoavnash@users.noreply.github.com>
2022-05-31 16:24:59 +0200

    Update message_broker.py

--- marketplace_standard_app_api/models/message_broker.py
+++ marketplace_standard_app_api/models/message_broker.py
@@ -7,14 +7,10 @@ class MessageBrokerRequestModel(BaseModel):
     endpoint: str = Field(None, description="API endpoint of the application")
     query_params: Optional[Dict[str, str]] = Field(None, description="Query parameters")
     headers: Optional[Dict[str, str]] = Field(None, description="Request headers")
-    body: Optional[str] = Field(
-        None, description="The request message body encoded in base64"
-    )
+    body: Optional[str] = Field(None, description="The request message body")
 
 
 class MessageBrokerResponseModel(BaseModel):
     status_code: int = Field(200, description="The HTTP response code")
     headers: Optional[Dict[str, str]] = Field(None, description="Response headers")
-    body: Optional[str] = Field(
-        None, description="The response message body encoded in base64"
-    )
+    body: Optional[str] = Field(None, description="The response message body")
