14f648a88534058997c4ecf385c481883587f621
nash <62147380+yoavnash@users.noreply.github.com>
2022-05-31 09:51:35 +0200

    Update message_broker.py

--- marketplace_standard_app_api/models/message_broker.py
+++ marketplace_standard_app_api/models/message_broker.py
@@ -5,14 +5,12 @@ from pydantic import BaseModel, Field
 
 class MessageBrokerRequestModel(BaseModel):
     endpoint: str = Field(None, description="API endpoint of the application")
-    body_base64: str = Field(None, description="A base64-encrypted body")
-    body: Optional[Any] = Field(None, description="The request message payload")
     query_params: Optional[Dict[str, str]] = Field(None, description="Query parameters")
     headers: Optional[Dict[str, str]] = Field(None, description="Request headers")
+    body: Optional[str] = Field(None, description="The request message body encoded in base64")
 
 
 class MessageBrokerResponseModel(BaseModel):
     status_code: int = Field(200, description="The HTTP response code")
-    body_base64: str = Field(None, description="A base64-encrypted body")
-    body: Optional[Any] = Field(None, description="The response message payload")
     headers: Optional[Dict[str, str]] = Field(None, description="Response headers")
+    body: Optional[str] = Field(None, description="The response message body encoded in base64")
