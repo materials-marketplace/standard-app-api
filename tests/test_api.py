192e0657f4e9289e3ae43f05eec72000205eac9a
Carl Simon Adorf <simon.adorf@epfl.ch>
2022-06-29 20:12:52 +0200

    Implement tests for transformation capabilities.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,6 @@
 import json
+from time import sleep
+from uuid import UUID
 
 import pytest
 from fastapi import Request
@@ -8,6 +10,7 @@ import marketplace_standard_app_api.database
 import marketplace_standard_app_api.routers.object_storage
 from marketplace_standard_app_api import api
 from marketplace_standard_app_api.main import auth_token_bearer
+from marketplace_standard_app_api.models.transformation import TransformationModel
 
 
 async def _fake_auth_token_bearer(request: Request):
@@ -33,6 +36,17 @@ def client(tmp_path, monkeypatch):
     with client:
         yield client
     api.dependency_overrides = {}
+
+
+@pytest.fixture(autouse=True)
+def _mock_simulation_dir(monkeypatch, tmp_path):
+    import marketplace_standard_app_api.reference.simulation_controller.simulation
+
+    monkeypatch.setattr(
+        marketplace_standard_app_api.reference.simulation_controller.simulation,
+        "SIMULATIONS_FOLDER_PATH",
+        tmp_path / "simulations",
+    )
 
 
 def test_frontend(client):
@@ -121,3 +135,69 @@ def test_create_and_delete_json_dataset(client, collection, tmp_path):
     assert client.head(f"/data/{collection}/test.json").status_code == 404
     assert client.get(f"/data/{collection}/test.json").status_code == 404
     assert client.get(f"/data/{collection}").status_code == 204
+
+
+def test_access_non_existing_transformation(client):
+    non_existent_transformation_id = UUID("63ed5190-82cb-4f02-88ce-5644faf6d876")
+    response = client.get(f"/transformations/{non_existent_transformation_id}")
+    assert response.status_code == 404
+    assert not response.ok
+
+
+def test_create_and_runtransformation(client):
+    response = client.get("transformations")
+    assert len(response.json()["items"]) == 0
+    assert response.status_code == 200
+    assert response.ok
+
+    response = client.post("/transformations", json={"parameters": {"input_value": 10}})
+    assert response.status_code == 200
+    transformation_id = response.json()["id"]
+
+    response = client.get("transformations")
+    assert len(response.json()["items"]) == 1
+    transformation = TransformationModel(**response.json()["items"][0])
+    assert str(transformation.id) == transformation_id
+    assert response.status_code == 200
+    assert response.ok
+
+    response = client.get(f"/transformations/{transformation_id}")
+    assert response.json()["id"] == transformation_id
+    assert response.json()["state"] == "CREATED"
+    assert response.ok
+
+    response = client.get(f"/transformations/{transformation_id}/state")
+    assert response.json()["id"] == transformation_id
+    assert response.json()["state"] == "CREATED"
+
+    response = client.patch(
+        f"/transformations/{transformation_id}", json={"state": "CREATED"}
+    )
+    assert response.status_code == 422
+
+    response = client.patch(
+        f"/transformations/{transformation_id}", json={"state": "STOPPED"}
+    )
+    assert response.status_code == 409
+
+    response = client.patch(
+        f"/transformations/{transformation_id}", json={"state": "RUNNING"}
+    )
+    assert response.ok
+    assert response.json()["id"] == transformation_id
+    assert response.json()["state"] == "RUNNING"
+
+    sleep(3)
+
+    response = client.get(f"/transformations/{transformation_id}")
+    assert response.ok
+    assert response.json()["id"] == transformation_id
+    assert response.json()["state"] == "COMPLETED"
+
+    response = client.delete(f"/transformations/{transformation_id}")
+    assert response.status_code == 204
+    assert response.ok
+
+    response = client.get(f"/transformations/{transformation_id}")
+    assert response.status_code == 404
+    assert not response.ok
